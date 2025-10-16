3ebcc0016c787868a75a802ed6bc30d4f7809403
OleBB <6317651+OleBB@users.noreply.github.com>
2025-10-16 11:32:32 +0200

    funker itl å lese av 1 lvm fil nå

--- wavescripts/pressure_pro.py
+++ wavescripts/pressure_pro.py
@@ -13,15 +13,15 @@ def read_lvm(filename, skip_lines_after_header=0):
     # Read file once
     with open(filename, 'r', encoding='latin-1') as f:
         lines = f.readlines()
-        print('first line: ', lines[0])
-        for line in lines[:30]: print(line.strip())
+        #print('kommentar: ', lines[23:24])
+        #for line in lines[:30]: print(line.strip())
 
         # Find header end
         for i, line in enumerate(lines):
             if line.strip().startswith('X_Value'): #X_value er navnet på første kolonne
                 start_index = i + 2
                 print('startindex er ', start_index)
-                print('startindex-1=', lines[start_index-1])
+                print('Kommentar=', lines[start_index-1])
                 break
 
     # Skip additional lines after header
@@ -49,17 +49,30 @@ files = sorted(data_folder.glob("*.lvm"))
 print(f"Found {len(files)} .lvm files")
 #%%
 # Process files (set skip_lines_after_header to desired number, e.g., 2)
-df = pd.concat(
-    [read_lvm(f, skip_lines_after_header=2) for f in files[:1]],  # Change files[:1] to files for all
-    ignore_index=True
-)
+#df = pd.concat(
+#    [read_lvm(f, skip_lines_after_header=2) for f in files[:1]],  # Change files[:1] to files for all
+#    ignore_index=True
+#)
 
+amps = df['mA']
+amps = amps[(amps >= 0.002) & (amps <= 0.02)] *1000
+ 
+"""Use .where() when you want to preserve time/index continuity — useful for time series, 
+so you can see where the dropouts occurred."""
 # Convert to mA
-#df['mA'] =  df['mA'].where(df['mA'] != 0) df['mA'] * 1000
+#df['mA'] =  df['mA'].where(df['mA'] != 0) * 1000
+#df = df[amps.values != 0]
+#amps.where(amps !=0).mean(trim(0.0))
 #sales.where(sales != 0).mean(trim=0.1) * 1000
 
-mAserie = df['mA']
-snittet = mAserie.mean()
+
+"""If you want to cap the values instead of removing them:
+amps = amps.clip(lower=-0.01, upper=0.01)"""
+# drop rows with all zeros
+#df = df.loc[(df!=0).any(axis=1)] #trenger ikke den any trur eg.
+
+
+snittet = amps.mean()
 print('snittet er ', snittet)
 
 # Save to CSV
@@ -69,14 +82,19 @@ print('snittet er ', snittet)
 # Plot exmAle
 import matplotlib.pyplot as plt
 #df['mA'].clip(lower=5, upper=95)
-df['mA'].iloc[0:3000].plot()  # Line plot of A only
+amps.iloc[0:2000].plot()  # Line plot of A only
 #df.iloc[0:3].plot()
-plt.show()
+
+
+"""
+If you’re working with large time-series data, you can downsample before plotting:
+df["mA"].iloc[::100].plot()  # plot every 100th point"""
 
 #plt.figure(figsize=(10, 6))
 #plt.
-#plt.xlabel("Time [s]")
-#plt.ylabel("Current [mA]")
+plt.xlabel("Time [ms]")
+plt.ylabel("Current [mA]")
+plt.show()
 #plt.title("All LVM Files")
 #plt.legend()
 #plt.tight_layout()
